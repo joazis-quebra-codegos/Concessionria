7dd03a869f6dec2cf01e4aa6a74fdd652a61851c
joazis-quebra-codegos <joao.ds.zantti@gmail.com>
2026-04-07 19:42:07 -0300

    commit

---
+++ src/main/java/org/example/concessionariajpa/model/Carro.java
@@ -0,0 +1,5 @@
+package org.example.concessionariajpa.model;
+
+
+public class Carro {
+}
---
+++ src/test/java/org/example/concessionariajpa/ConcessionariaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.concessionariajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
